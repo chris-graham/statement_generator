fe32bd7584f96ef2b6c9bb9ad62f2966b5e48e4b
cgraham <chris_graham@acm.org>
2018-02-16 06:14:40 +0000

    load data from all files into database

--- src/store_ezpass_data.py
+++ src/store_ezpass_data.py
@@ -1,7 +1,10 @@
 import datetime
+import os
 import pandas as pd
 import psycopg2
 from sqlalchemy import *
+
+DATA_PATH = '/home/ubuntu/projects/statement_generator/data/ezpass/'
 
 # Cleanup data from CSV file
 def transform_ezpass_data(filename):
@@ -28,8 +31,16 @@ def transform_ezpass_data(filename):
 def convert_date_format(date):
     return datetime.datetime.strptime(date, '%m/%d/%Y').strftime('%Y/%m/%d')
 
+def backup_file(filename):
+    pass
+
 # Connect to database
 engine = create_engine('postgresql+psycopg2:///vervoer')
-table_data = transform_ezpass_data('/home/ubuntu/projects/statement_generator/data/ezpass/ezpass.csv')
-# Dump data from dataframe to database
-table_data.to_sql('ezpass', engine, if_exists='append')
+# Get all data filenames from data directory
+filenames = os.listdir(DATA_PATH)
+
+for filename in filenames:
+    print('loading data from ' + filename)
+    table_data = transform_ezpass_data(DATA_PATH + filename)
+    # Dump data from dataframe to database
+    table_data.to_sql('ezpass', engine, if_exists='append', index=False)
