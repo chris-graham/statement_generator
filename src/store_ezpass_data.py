ecc9d0253a899cdc237e1389bee70113752c3490
cgraham <chris_graham@acm.org>
2018-02-16 07:48:47 +0000

    backup files to S3 and delete from server

--- src/store_ezpass_data.py
+++ src/store_ezpass_data.py
@@ -1,9 +1,12 @@
+import boto3
+import botocore
 import datetime
 import os
 import pandas as pd
 import psycopg2
 from sqlalchemy import *
 
+BUCKET_NAME = 'vervoer-statement-generator-backup'
 DATA_PATH = '/home/ubuntu/projects/statement_generator/data/ezpass/'
 
 # Cleanup data from CSV file
@@ -31,7 +34,7 @@ def transform_ezpass_data(filename):
 def convert_date_format(date):
     return datetime.datetime.strptime(date, '%m/%d/%Y').strftime('%Y/%m/%d')
 
-def backup_file(filename):
+def delete_file(filepath):
     pass
 
 # Connect to database
@@ -39,8 +42,13 @@ engine = create_engine('postgresql+psycopg2:///vervoer')
 # Get all data filenames from data directory
 filenames = os.listdir(DATA_PATH)
 
+s3 = boto3.client('s3')
+
 for filename in filenames:
-    print('loading data from ' + filename)
     table_data = transform_ezpass_data(DATA_PATH + filename)
     # Dump data from dataframe to database
     table_data.to_sql('ezpass', engine, if_exists='append', index=False)
+    # Backup file to S3
+    s3.upload_file(DATA_PATH + filename, BUCKET_NAME, 'ezpass/' + filename)
+    # Delete file from server
+    os.remove(DATA_PATH + filename)
